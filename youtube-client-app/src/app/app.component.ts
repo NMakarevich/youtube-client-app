ccb29740beb53263f9f88a4d07b6760d46191eeb
NMakarevich <nikoladze529@mail.ru>
2023-10-19 17:39:14 +0300

    feat: implement main page layout

--- youtube-client-app/src/app/app.component.ts
+++ youtube-client-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SearchResponse } from './components/search/search-response';
+import { SortFilter } from './components/sorting-settings/sorting-settings.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youtube-client-app';
+
+  results!: SearchResponse;
+
+  sortFilter!: SortFilter;
+
+  getSearchTerm(response: SearchResponse) {
+    this.results = response;
+  }
+
+  getSortFilter(sortFilter: SortFilter) {
+    if (!this.sortFilter) this.sortFilter = sortFilter;
+    else Object.assign(this.sortFilter, sortFilter);
+  }
 }
--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultsModule } from './components/results/results.module';
+import { ButtonComponent } from './components/UI/button/button.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ResultsModule,
+    ButtonComponent,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
