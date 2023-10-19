20ab19d8c5c1655d069ffe52a5df44b7855d98cc
NMakarevich <nikoladze529@mail.ru>
2023-10-19 17:38:36 +0300

    feat: implement header component

--- youtube-client-app/src/app/components/header/header.component.ts
+++ youtube-client-app/src/app/components/header/header.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { SearchResponse } from '../search/search-response';
+import { SortFilter } from '../sorting-settings/sorting-settings.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  settingsIsOpen = false;
+
+  @Output() setSearchResults: EventEmitter<SearchResponse> =
+    new EventEmitter<SearchResponse>();
+
+  @Output() setSortFilter: EventEmitter<SortFilter> =
+    new EventEmitter<SortFilter>();
+
+  getSearchResults(response: SearchResponse) {
+    this.setSearchResults.emit(response);
+  }
+
+  getSortFilter(sortFilter: SortFilter) {
+    this.setSortFilter.emit(sortFilter);
+  }
+
+  toggleSettings() {
+    this.settingsIsOpen = !this.settingsIsOpen;
+  }
+}
