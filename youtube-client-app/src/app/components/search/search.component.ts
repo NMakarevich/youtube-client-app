76ed48e420cc5f11bf198b2238de18735041027d
NMakarevich <nikoladze529@mail.ru>
2023-10-19 11:27:34 +0300

    feat: implement search component

--- youtube-client-app/src/app/components/search/search.component.ts
+++ youtube-client-app/src/app/components/search/search.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { SearchResponse } from './search-response';
+import { results } from '../../mock/mock';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {}
+export class SearchComponent {
+  searchTerm = '';
+
+  searchResults!: SearchResponse;
+
+  @ViewChild('searchInput') searchInput!: ElementRef;
+
+  @Output() showResults: EventEmitter<SearchResponse> =
+    new EventEmitter<SearchResponse>();
+
+  search() {
+    this.searchTerm = this.searchInput.nativeElement.value;
+    this.searchResults = results;
+    if (this.searchTerm.length) this.showResults.emit(this.searchResults);
+  }
+}
