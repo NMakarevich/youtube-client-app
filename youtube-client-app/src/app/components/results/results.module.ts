c753a00dae7b50b804b4eb1633cce03415c8546e
NMakarevich <nikoladze529@mail.ru>
2023-10-19 19:23:58 +0300

    feat: implement colorizeBorder directive

--- youtube-client-app/src/app/components/results/results.module.ts
+++ youtube-client-app/src/app/components/results/results.module.ts
@@ -4,9 +4,14 @@ import { ResultsListComponent } from './results-list/results-list.component';
 import { ResultsItemComponent } from './results-item/results-item.component';
 import { ButtonComponent } from '../UI/button/button.component';
 import { SharedModule } from '../../shared/shared.module';
+import { ColorizeBorderDirective } from '../../directives/colorize-border.directive';
 
 @NgModule({
-  declarations: [ResultsListComponent, ResultsItemComponent],
+  declarations: [
+    ResultsListComponent,
+    ResultsItemComponent,
+    ColorizeBorderDirective,
+  ],
   exports: [ResultsListComponent],
   imports: [CommonModule, ButtonComponent, SharedModule],
 })
---
+++ youtube-client-app/src/app/directives/colorize-border.directive.ts
@@ -0,0 +1,39 @@
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
+
+const [week, month, halfOfYear] = [7, 30, 180].map(
+  (i) => i * 24 * 60 * 60 * 1000
+);
+
+enum COLORS {
+  RED = '#F44336',
+  YELLOW = '#FFEB3B',
+  GREEN = '#4CAF50',
+  BLUE = '#2196F3',
+}
+
+@Directive({
+  selector: '[appColorizeBorder]',
+})
+export class ColorizeBorderDirective implements OnInit {
+  @Input() appColorizeBorder!: string;
+
+  constructor(
+    private readonly elemRef: ElementRef,
+    private readonly renderer: Renderer2
+  ) {}
+
+  ngOnInit() {
+    const dateNow = new Date().getTime();
+    const timeDifference = dateNow - new Date(this.appColorizeBorder).getTime();
+    let color;
+    if (timeDifference <= week) color = COLORS.BLUE;
+    else if (timeDifference <= month) color = COLORS.GREEN;
+    else if (timeDifference <= halfOfYear) color = COLORS.YELLOW;
+    else color = COLORS.RED;
+    this.renderer.setStyle(
+      this.elemRef.nativeElement,
+      'border-bottom',
+      `3px solid ${color}`
+    );
+  }
+}
