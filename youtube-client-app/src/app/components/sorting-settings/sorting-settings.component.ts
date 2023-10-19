dfa401a4d3513accbe26d72a2c0ab7df618621ec
NMakarevich <nikoladze529@mail.ru>
2023-10-19 17:21:21 +0300

    refactor: update sorting methods

--- youtube-client-app/src/app/components/sorting-settings/sorting-settings.component.ts
+++ youtube-client-app/src/app/components/sorting-settings/sorting-settings.component.ts
@@ -17,11 +17,13 @@ export class SortingSettingsComponent {
 
   changeDateDirection() {
     this.sortFilter.date = this.sortFilter.date <= 0 ? 1 : -1;
+    this.sortFilter.views = 0;
     this.setSortFilter.emit(this.sortFilter);
   }
 
   changeViewsDirection() {
     this.sortFilter.views = this.sortFilter.views <= 0 ? 1 : -1;
+    this.sortFilter.date = 0;
     this.setSortFilter.emit(this.sortFilter);
   }
 
