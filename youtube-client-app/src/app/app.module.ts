2630e923d4f4cd31885ff622e183394d4053af50
NMakarevich <nikoladze529@mail.ru>
2023-10-20 23:14:19 +0300

    refactor: update module

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ResultsModule } from './components/results/results.module';
-import { ButtonComponent } from './components/UI/button/button.component';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ResultsModule,
-    ButtonComponent,
-    SharedModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -1,15 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from '../components/header/header.component';
-import { SortingSettingsModule } from '../components/sorting-settings/sorting-settings.module';
-import { SearchModule } from '../components/search/search.module';
-import { ButtonComponent } from '../components/UI/button/button.component';
-import { SortPipe } from '../pipes/sort.pipe';
-import { FilterPipe } from '../pipes/filter.pipe';
 
 @NgModule({
-  declarations: [HeaderComponent, SortPipe, FilterPipe],
-  exports: [HeaderComponent, SortPipe, FilterPipe],
-  imports: [CommonModule, SortingSettingsModule, SearchModule, ButtonComponent],
+  declarations: [],
+  exports: [],
+  imports: [CommonModule],
 })
 export class SharedModule {}
