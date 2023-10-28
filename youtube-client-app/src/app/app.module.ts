00d5d15290e92d423ae0f9af837318b43840549d
NMakarevich <nikoladze529@mail.ru>
2023-10-28 13:15:24 +0300

    refactor: delete auth module from imports

--- youtube-client-app/src/app/app.module.ts
+++ youtube-client-app/src/app/app.module.ts
@@ -5,17 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
-import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    CoreModule,
-    AuthModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, SharedModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
