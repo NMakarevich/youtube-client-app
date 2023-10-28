ab99535bd8e2ddd797e8b53437641e5941fa2682
NMakarevich <nikoladze529@mail.ru>
2023-10-28 13:13:41 +0300

    refactor: move auth service to shared module

--- youtube-client-app/src/app/auth/auth.module.ts
+++ youtube-client-app/src/app/auth/auth.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
-import { AuthService } from './services/auth.service';
 import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [AuthPageComponent, LoginComponent],
   imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
-  providers: [AuthService],
+  providers: [],
 })
 export class AuthModule {}
--- youtube-client-app/src/app/shared/shared.module.ts
+++ youtube-client-app/src/app/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../auth/services/auth.service';
 
 @NgModule({
   declarations: [],
   exports: [],
   imports: [CommonModule],
+  providers: [AuthService],
 })
 export class SharedModule {}
