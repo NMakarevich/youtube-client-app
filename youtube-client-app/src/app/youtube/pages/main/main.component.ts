897b47a16cc5c2267436747edf5fe9a82824f902
NMakarevich <nikoladze529@mail.ru>
2023-10-29 11:30:11 +0300

    refactor: delete unused code

--- youtube-client-app/src/app/youtube/pages/main/main.component.ts
+++ youtube-client-app/src/app/youtube/pages/main/main.component.ts
@@ -1,23 +1,8 @@
 import { Component } from '@angular/core';
-import { YoutubeService } from '../../services/youtube.service';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
-  constructor(private readonly youtubeService: YoutubeService) {}
-
-  get response() {
-    return this.youtubeService.response;
-  }
-
-  get sort() {
-    return this.youtubeService.sort;
-  }
-
-  get filter() {
-    return this.youtubeService.filter;
-  }
-}
+export class MainComponent {}
