7d9b3284a541a1db30dc95a98a1fbd3c873e0970
NMakarevich <nikoladze529@mail.ru>
2023-11-16 19:32:39 +0300

    refactor: add button type

--- youtube-client-app/src/app/core/components/UI/button/button.component.ts
+++ youtube-client-app/src/app/core/components/UI/button/button.component.ts
@@ -1,5 +1,11 @@
 import { Component, Input } from '@angular/core';
 
+export enum ButtonType {
+  'button',
+  'submit',
+  'reset',
+}
+
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
@@ -8,4 +14,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ButtonComponent {
   @Input() disabled = false;
+
+  @Input() type: ButtonType = ButtonType.button;
 }
