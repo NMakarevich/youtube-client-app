357c90f6db71f90769759c0f4f0d81c1372ed5e3
NMakarevich <nikoladze529@mail.ru>
2023-11-16 16:59:06 +0300

    feat: add favorite button

---
+++ youtube-client-app/src/app/youtube/components/ui/favorite-button/favorite-button.component.ts
@@ -0,0 +1,24 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Store } from '@ngrx/store';
+import { toggleFavorite } from '../../../../redux/actions/favorites.action';
+import { selectFavoritesIds } from '../../../../redux/reducers/favorites.reducer';
+
+@Component({
+  selector: 'app-favorite-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './favorite-button.component.html',
+  styleUrls: ['./favorite-button.component.scss'],
+})
+export class FavoriteButtonComponent {
+  @Input() id!: string;
+
+  favorites$ = this.store.select(selectFavoritesIds);
+
+  constructor(private readonly store: Store) {}
+
+  toggleFavorite() {
+    this.store.dispatch(toggleFavorite({ id: this.id }));
+  }
+}
